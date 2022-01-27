d40a6594b2fa90d4b53997cce04995db4c8800c3
Kiran Kumari <kiran1999rp@gmail.com>
2022-01-27 19:25:37 +0530

    change default image to download

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
             gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
             return gravatar_url
         end
-        image_path("default_user.png")
+        image_path("download.png")
     end
 
 end
